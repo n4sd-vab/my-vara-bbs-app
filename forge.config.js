c86131e5356e0533937c216331748e535dbb0be5
Sperry Davis <sperrydd@cox.net>
2026-02-28 22:22:45 -0500

    updated for publishing

--- forge.config.js
+++ forge.config.js
@@ -41,4 +41,17 @@ module.exports = {
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'n4sd-vab',
+          name: 'my-vara-bbs-app'
+        },
+        prerelease: false,
+        draft: false
+      },
+    },
+  ], 
 };
